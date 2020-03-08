dce5f6a5d970ed2e5e37f7fb601a45cbe6ddca3c
Cameron Reikes <cameronreikes@gmail.com>
2020-03-08 15:08:40 -0700

    Add suitcase takedown area and cooldown

--- src/Player.gd
+++ src/Player.gd
@@ -4,7 +4,11 @@ const player_state = preload("res://player_state.tres")
 const middle = 960.0
 const step = 200.0
 
+
 onready var target_x_pos: float = global_position.x
+
+var target_speed := 700.0
+var cur_speed := 0.0
 
 func _input(event):
 	if event.is_action_pressed("g_left"):
@@ -19,12 +23,17 @@ func _input(event):
 			target_x_pos += step
 
 func take_down_suitcase():
-	pass
+	if cur_speed < 0.70*target_speed: # can only takedown when at speed
+		return
+	cur_speed = target_speed*0.3
+	$SuitcaseTakedown.takedown(on_left_side())
 
 func _physics_process(delta):
 	global_position.x = ((target_x_pos - global_position.x) * 15.0 * delta) + global_position.x
 	
-	player_state.move(Vector2(0, -500)*delta)
+	cur_speed = ((target_speed - cur_speed) * 2.5 * delta) + cur_speed
+	
+	player_state.move(Vector2(0, -cur_speed)*delta)
 
 func on_left_side() -> bool:
 	return target_x_pos < middle
---
+++ src/SuitcaseTakedown.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+func _ready():
+	$CollisionShape2D.disabled = true
+
+func takedown(on_left_side: bool):
+	if on_left_side:
+		position.x = -$CollisionShape2D.shape.extents.x
+	else:
+		position.x = $CollisionShape2D.shape.extents.x
+	$CollisionShape2D.disabled = false
+	$Timer.start()
+
+func _on_Timer_timeout():
+	$CollisionShape2D.disabled = true
